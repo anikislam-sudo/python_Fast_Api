cbe1ed32bddc579f4b5a9824107aff1985c2e738
Anik Islam <islammoshiu@gmail.com>
2024-08-21 23:19:43 +0600

    folder structure,database connect & create model

---
+++ app/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column,Integer,Float,String,DATE
+from databases import Base
+
+class User(Base):
+     __tablename__ = 'courses'  
+     
+id = Column(Integer, primary_key=True, index=True)
+title = Column(String, index=True)
+description = Column(String)
+instructor = Column(String)
+duration = Column(Integer)
+price = Column(Float)
+
+class Enrollment(Base):
+    __tablename__ = 'enrollments'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    student_name = Column(String)
+    course_id = Column(Integer)
+    enrollment_date = Column(Date)
